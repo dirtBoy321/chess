827fcb8fba4dc5c548fdd3091042bc20267a3ea8
dirtBoy321 <124312851+dirtBoy321@users.noreply.github.com>
2023-09-29 13:05:02 -0400

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -1,14 +1,30 @@
 package chess;
 
-public class Queen extends ReturnPiece{
-    public Queen(PieceType type,PieceFile file,int rank){
-        pieceType=type;
-        pieceFile=file;
-        pieceRank =rank;
+import chess.ReturnPiece.PieceType;
+
+public class Queen extends Piece {
+    public Queen(Color player,int  file,int rank){
+        player=this.player;
+        fileIndex=file;
+        rankIndex =rank;
 
     }
-    boolean isLegal(PieceFile file,int rank){
-        
-        return false;
+    ReturnPiece getReturnPiece(){
+        ReturnPiece tempPiece = new ReturnPiece();
+        tempPiece.pieceFile=getFile(fileIndex);
+        tempPiece.pieceRank=getrank(rankIndex);
+        if(player.equals(Color.white)){
+            tempPiece.pieceType=PieceType.WQ;
+        }else{
+            tempPiece.pieceType=PieceType.BQ;
+        }
+        return tempPiece;
     }
+
+
+     boolean isLegal(int newFilefileIndex, int newRankIndex){
+        
+        return true;
+    }
+    
 }
