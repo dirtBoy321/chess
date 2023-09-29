fb2f6c9594348cf48f4ac216adfb9fb00a90cf99
dirtBoy321 <124312851+dirtBoy321@users.noreply.github.com>
2023-09-29 13:05:45 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,15 +1,37 @@
 package chess;
 
-public class Knight extends ReturnPiece {
-    public Knight(PieceType type,PieceFile file,int rank){
-        pieceType=type;
-        pieceFile=file;
-        pieceRank =rank;
+import chess.ReturnPiece.PieceType;
+
+public class Knight extends Piece {
+    public Knight(Color player,int  file,int rank){
+        player=this.player;
+        fileIndex=file;
+        rankIndex =rank;
 
     }
+    ReturnPiece getReturnPiece(){
+        ReturnPiece tempPiece = new ReturnPiece();
+        tempPiece.pieceFile=getFile(fileIndex);
+        tempPiece.pieceRank=getrank(rankIndex);
+        if(player.equals(Color.white)){
+            tempPiece.pieceType=PieceType.WN;
+        }else{
+            tempPiece.pieceType=PieceType.BN;
+        }
+        return tempPiece;
+    }
 
-    boolean isLegal(PieceFile file,int rank){
+
+    boolean isLegal(int newFilefileIndex, int newRankIndex){
+         if(((rankIndex+1==newRankIndex||rankIndex-1==newRankIndex)&&(fileIndex+2==newRankIndex||fileIndex-2==newRankIndex))||
+            ((rankIndex+2==newRankIndex||rankIndex-2==newRankIndex)&&(fileIndex+1==newRankIndex||fileIndex-1==newRankIndex))){
+             return true;
+         }else{
+             return false;
+         }
         
-        return false;
+       
     }
+    
 }
+
