00f17571ae7d0cc36e86ad2c05069897806e4600
dirtBoy321 <124312851+dirtBoy321@users.noreply.github.com>
2023-09-29 13:06:43 -0400

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,17 +1,30 @@
 package chess;
 
+import chess.ReturnPiece.PieceType;
 
-public class Bishop extends ReturnPiece {
-    public Bishop(PieceType type,PieceFile file,int rank){
-        pieceType=type;
-        pieceFile=file;
-        pieceRank =rank;
+public class Bishop extends Piece {
+    public Bishop(Color player,int  file,int rank){
+        player=this.player;
+        fileIndex=file;
+        rankIndex =rank;
 
     }
+    ReturnPiece getReturnPiece(){
+        ReturnPiece tempPiece = new ReturnPiece();
+        tempPiece.pieceFile=getFile(fileIndex);
+        tempPiece.pieceRank=getrank(rankIndex);
+        if(player.equals(Color.white)){
+            tempPiece.pieceType=PieceType.WB;
+        }else{
+            tempPiece.pieceType=PieceType.BB;
+        }
+        return tempPiece;
+    }
 
-     boolean isLegal(PieceFile file,int rank){
+
+     boolean isLegal(int newFilefileIndex, int newRankIndex){
         
-        return false;
+        return true;
     }
     
 }
