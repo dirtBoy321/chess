cb19dfd02ccba7b9febbd012db6b8d93b3c31d7d
dirtBoy321 <124312851+dirtBoy321@users.noreply.github.com>
2023-09-29 13:06:12 -0400

    Update King.java

--- King.java
+++ King.java
@@ -1,15 +1,30 @@
 package chess;
 
-public class King extends ReturnPiece {
-    public King(PieceType type,PieceFile file,int rank){
-        pieceType=type;
-        pieceFile=file;
-        pieceRank =rank;
+import chess.ReturnPiece.PieceType;
+
+public class King extends Piece {
+    public King(Color player,int  file,int rank){
+        player=this.player;
+        fileIndex=file;
+        rankIndex =rank;
 
     }
 
-    boolean isLegal(PieceFile file,int rank){
+    ReturnPiece getReturnPiece(){
+        ReturnPiece tempPiece = new ReturnPiece();
+        tempPiece.pieceFile=getFile(fileIndex);
+        tempPiece.pieceRank=getrank(rankIndex);
+        if(player.equals(Color.white)){
+            tempPiece.pieceType=PieceType.WK;
+        }else{
+            tempPiece.pieceType=PieceType.BK;
+        }
+        return tempPiece;
+    }
+
+     boolean isLegal(int newFilefileIndex, int newRankIndex){
         
-        return false;
+        return true;
     }
+    
 }
