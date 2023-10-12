08f7ffc5972cf44d3d354114d5ba0c4fd47cf647
dirtBoy321 <124312851+dirtBoy321@users.noreply.github.com>
2023-10-12 11:10:27 -0400

    Update Pawn.java
    pawn is now completely working

--- Pawn.java
+++ Pawn.java
@@ -4,7 +4,7 @@ import chess.ReturnPiece.PieceType;
 
 public class Pawn extends Piece {
     public Pawn(Color player,int  file,int rank){
-        player=this.player;
+        this.player=player;
         fileIndex=file;
         rankIndex =rank;
 
@@ -21,10 +21,54 @@ public class Pawn extends Piece {
         return tempPiece;
     }
 
+    //still need tocheck path for all ifs in method
+     boolean isLegal(int newFileIndex, int newRankIndex){
+        if(player==Color.white){
+            //first move of two  for white
+            if((rankIndex==6)&&(newRankIndex==4)&&(fileIndex==newFileIndex)){
+                if(Chess.bored[5][fileIndex]==null&&Chess.bored[4][fileIndex]==null){
+                    return true;
+                }
+            }
+            //if check for one ahead
+            if((newRankIndex==rankIndex-1)&&(fileIndex==newFileIndex)){
+                if(Chess.bored[newRankIndex][newFileIndex]==null){
+                    return true;
+                }
+            }
+            //if check for  diagnal attack
+            if((newRankIndex==rankIndex-1)&&(newFileIndex==fileIndex+1||newFileIndex==fileIndex+1)){
+                if(Chess.bored[newRankIndex][newFileIndex]!=null){
+                    if(Chess.bored[newRankIndex][newFileIndex].player==Color.black){
+                        return true;
+                    }
+                }
+                
+            }
 
-     boolean isLegal(int newFilefileIndex, int newRankIndex){
-        
-        return true;
+        }else if(player==Color.black){//player is black 
+            if((rankIndex==1)&&(newRankIndex==3)&&(fileIndex==newFileIndex)){
+                if(Chess.bored[2][fileIndex]==null&&Chess.bored[3][fileIndex]==null){
+                    return true;
+                }
+            }
+            //if check for one ahead 
+            if((newRankIndex==rankIndex+1)&&(fileIndex==newFileIndex)){
+               if(Chess.bored[newRankIndex][newFileIndex]==null){
+                    return true;
+                }
+            }
+            //if check for  diagnal attack
+            if((newRankIndex==rankIndex+1)&&(newFileIndex==fileIndex+1||newFileIndex==fileIndex+1)){
+                if(Chess.bored[newRankIndex][newFileIndex]!=null){
+                    if(Chess.bored[newRankIndex][newFileIndex].player==Color.white){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
+    
     
 }
