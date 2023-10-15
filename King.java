d633a4ca7ff196080a85051c2cd7059f7450119e
dirtBoy321 <124312851+dirtBoy321@users.noreply.github.com>
2023-10-14 22:12:27 -0400

    Update King.java

--- King.java
+++ King.java
@@ -3,10 +3,13 @@ package chess;
 import chess.ReturnPiece.PieceType;
 
 public class King extends Piece {
+    
+
     public King(Color player,int  file,int rank){
         this.player=player;
         fileIndex=file;
         rankIndex =rank;
+       
 
     }
 
@@ -23,6 +26,43 @@ public class King extends Piece {
     }
 
      boolean isLegal(int newFileIndex, int newRankIndex){
+        //check for castle
+        if(moved==false){//white 
+            if(player==Color.white){
+                
+                if(Chess.bored[7][7]!=null&&newRankIndex==7&&newFileIndex==6){//right castle
+                    if(Chess.bored[7][7].moved==false&&Chess.bored[7][6]==null&&Chess.bored[7][5]==null){
+                        Chess.bored[7][7].movePiece(5,7);
+                        return true;
+                        
+
+                    }
+
+                }
+                if(Chess.bored[7][0]!=null&&newRankIndex==7&&newFileIndex==1){//left castle
+                    if(Chess.bored[7][0].moved==false&&Chess.bored[7][1]==null&&Chess.bored[7][2]==null&&Chess.bored[7][3]==null){
+                        Chess.bored[7][0].movePiece(2,7);
+                        return true;
+                    }
+                }
+            }else if(player==Color.black){
+                if(Chess.bored[0][7]!=null&&newRankIndex==0&&newFileIndex==6){//right castle
+                    if(Chess.bored[0][7].moved==false&&Chess.bored[0][6]==null&&Chess.bored[0][5]==null){
+                        Chess.bored[0][7].movePiece(5,0);
+                        return true;
+                    }
+                }
+                if(Chess.bored[0][0]!=null&&newRankIndex==0&&newFileIndex==1){//left castle
+                    if(Chess.bored[0][0].moved==false&&Chess.bored[0][1]==null&&Chess.bored[0][2]==null&&Chess.bored[0][3]==null){
+                        Chess.bored[0][0].movePiece(2,0);
+                        return true;
+                    }
+
+                }
+            }
+        }
+      ///////////////////////////////////////////////////////////////////////  
+
 
         if((newRankIndex==rankIndex+1)||(newRankIndex==rankIndex-1)||(newFileIndex==fileIndex-1)||(newFileIndex==fileIndex+1)){
             if(Chess.bored[newRankIndex][newFileIndex]!=null){
@@ -30,6 +70,7 @@ public class King extends Piece {
                     return false;
                 }
             }else{
+                
                 return true;
             }
         }
