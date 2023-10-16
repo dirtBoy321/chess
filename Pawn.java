3aa5189614e291f2677aa056da0f349ba7011dd3
dirtBoy321 <124312851+dirtBoy321@users.noreply.github.com>
2023-10-16 02:20:43 -0400

    with enpassant

--- Pawn.java
+++ Pawn.java
@@ -3,10 +3,13 @@ package chess;
 import chess.ReturnPiece.PieceType;
 
 public class Pawn extends Piece {
+    
+
     public Pawn(Color player,int  file,int rank){
         this.player=player;
         fileIndex=file;
         rankIndex =rank;
+        
 
     }
     ReturnPiece getReturnPiece(){
@@ -27,12 +30,14 @@ public class Pawn extends Piece {
             //first move of two  for white
             if((rankIndex==6)&&(newRankIndex==4)&&(fileIndex==newFileIndex)){
                 if(Chess.bored[5][fileIndex]==null&&Chess.bored[4][fileIndex]==null){
+                    Chess.enpasantablPiece=Chess.bored[rankIndex][fileIndex];
                     return true;
                 }
             }
             //if check for one ahead
             if((newRankIndex==rankIndex-1)&&(fileIndex==newFileIndex)){
                 if(Chess.bored[newRankIndex][newFileIndex]==null){
+                    
                     return true;
                 }
             }
@@ -40,21 +45,37 @@ public class Pawn extends Piece {
             if((newRankIndex==rankIndex-1)&&(newFileIndex==fileIndex+1||newFileIndex==fileIndex-1)){
                 if(Chess.bored[newRankIndex][newFileIndex]!=null){
                     if(Chess.bored[newRankIndex][newFileIndex].player==Color.black){
+                        
                         return true;
                     }
                 }
                 
             }
+            //enpassont white
+            if(rankIndex==3&&newRankIndex==2){
+                if(fileIndex+1==newFileIndex&&Chess.bored[rankIndex][fileIndex+1].equals(Chess.enpasantablPiece)){
+                    Chess.bored[rankIndex][fileIndex+1]=null;
+                    return true;
+                }else if(fileIndex-1==newFileIndex&&Chess.bored[rankIndex][fileIndex-1].equals(Chess.enpasantablPiece)){
+                    Chess.bored[rankIndex][fileIndex-1]=null;
+                    return true;
+                }
+            }
+            
+            
+            
 
         }else if(player==Color.black){//player is black 
-            if((rankIndex==1)&&(newRankIndex==3)&&(fileIndex==newFileIndex)){
+            if((rankIndex==1)&&(newRankIndex==3)&&(fileIndex==newFileIndex)){ //move2 black
                 if(Chess.bored[2][fileIndex]==null&&Chess.bored[3][fileIndex]==null){
+                   Chess.enpasantablPiece=Chess.bored[rankIndex][fileIndex];
                     return true;
                 }
             }
             //if check for one ahead 
             if((newRankIndex==rankIndex+1)&&(fileIndex==newFileIndex)){
                if(Chess.bored[newRankIndex][newFileIndex]==null){
+                   
                     return true;
                 }
             }
@@ -62,10 +83,26 @@ public class Pawn extends Piece {
             if((newRankIndex==rankIndex+1)&&(newFileIndex==fileIndex+1||newFileIndex==fileIndex-1)){
                 if(Chess.bored[newRankIndex][newFileIndex]!=null){
                     if(Chess.bored[newRankIndex][newFileIndex].player==Color.white){
+                       
                         return true;
                     }
                 }
             }
+
+
+            //enpassont black
+            if(rankIndex==4&&newRankIndex==5){
+                if(fileIndex+1==newFileIndex&&Chess.bored[rankIndex][fileIndex+1].equals(Chess.enpasantablPiece)){
+                    Chess.bored[rankIndex][fileIndex+1]=null;
+                    return true;
+                }else if(fileIndex-1==newFileIndex&&Chess.bored[rankIndex][fileIndex-1].equals(Chess.enpasantablPiece)){
+                    Chess.bored[rankIndex][fileIndex-1]=null;
+                    return true;
+                }
+            }
+
+
+
         }
         return false;
     }
